38a188a9e13f39bf7902571807025f0bbcf0a07a
chris skinner <chrisskinner22@gmail.com>
2014-09-14 15:55:55 -0400

    hjsjhdavjadkj
    mndcmnadnac

--- cpd/application.js
+++ cpd/application.js
@@ -7,6 +7,7 @@ $(function() {
 		$(".navw2").toggleClass("navw2open"); 
 		$(".nav2").toggleClass("nav2open");
 		$(".block").toggleClass("block4");
+		$(".block2").toggleClass("block5");
 		$(".block3").toggleClass("block6"); 
 
 	});
