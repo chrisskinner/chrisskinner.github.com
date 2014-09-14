f181ee4a00b77a10a0e1f69dcaf8505f874ac048
chris skinner <chrisskinner22@gmail.com>
2014-09-14 02:38:30 -0400

    bvbjfvjk
    kjndavkjdvakjnda

---
+++ cpd/application.js
@@ -0,0 +1,17 @@
+
+//event handlers
+
+$(function() {
+
+	$('.menubutton').click(function(){
+		$(".navw2").toggleClass("navw2open"); 
+		$(".nav2").toggleClass("nav2open");
+		$(".block").toggleClass("block4");
+		$(".block2").toggleClass("block5");
+		$(".block3").toggleClass("block6"); 
+
+	});
+
+	
+
+});
