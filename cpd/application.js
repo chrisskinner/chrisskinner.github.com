4bd0ae4207e89db1784334f6de9b55e51d57121f
chris skinner <chrisskinner22@gmail.com>
2014-09-17 23:02:09 -0400

    ksvkjbvskj
    jdjksvjk

--- cpd/application.js
+++ cpd/application.js
@@ -1,4 +1,8 @@
-
+$(function(){
+$(".mworkinfo").hide();
+$(".mworkinfo2").hide();
+$('.mhideinfo').hide();
+});
 //event handlers
 
 $(function() {
@@ -10,6 +14,20 @@ $(function() {
 		$(".block2").toggleClass("block5");
 		$(".block3").toggleClass("block6"); 
 
+	});
+
+	$('.mshowinfo').click(function(){
+		$(".mworkinfo").show();
+		$(".mworkinfo2").show();
+		$(".mhideinfo").show();
+		$(".mshowinfo").hide();  
+	});
+
+	$('.mhideinfo').click(function(){
+		$(".mworkinfo").hide();
+		$(".mworkinfo2").hide();
+		$(".mhideinfo").hide();
+		$(".mshowinfo").show();  
 	});
 
 	
