7d3a64f7da754f90e91e5149a0cb15072e9f4206
chris skinner <chrisskinner22@gmail.com>
2015-04-14 00:33:53 -0400

    extrememoderation
    firstdraft

---
+++ extreme-moderation-website/application.js
@@ -0,0 +1,11 @@
+$(function() {
+ 
+    $('.menu-button').click(function(){
+        $(".bar1").toggleClass("bar3"); 
+        $(".bar2").toggleClass("bar4"); 
+        $(".nav-extended").toggleClass("nav-extended2"); 
+ 
+    });  
+});
+
+
